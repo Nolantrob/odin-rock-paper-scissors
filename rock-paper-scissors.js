3bda71a4843be50527a7b52c5d67c64208e759a8
Nolantrob <nolanrobertson97@gmail.com>
2025-02-20 16:23:29 -0500

    Properly update the score upon wins and losses

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,67 +1,100 @@
 const choiceButtons = document.querySelectorAll('.choice-button');
 
+let gameHasBegun = false;
+
 choiceButtons.forEach(button => {
     button.addEventListener('click', () => {
-        playGame(button.id);
+        playRound(button.id);
     })
 })
 
-const playGame = (humanChoice) => {
+const score = {
+    humanWins: 0,
+    computerWins: 0,
+    draws: 0
+}
+// Begin the game using the player's chosen hand
+function playRound(humanChoice) {
 
-    // This function generates rock, paper, or scissors randomly for the computer and returns the computer's choice.
+    let computerChoice;
+
     const getComputerChoice = () => {
         const options = ['rock', 'paper', 'scissors'];
-        const compNum = Math.floor(Math.random() * (options.length));
-        computerChoiceHolder = options[compNum];
-        return options[compNum];
-    }
+        return options[Math.floor(Math.random() * (options.length))];
+    }   
+    computerChoice = getComputerChoice();
 
-    const computerChoice = getComputerChoice();
-
-    let victoryStatement = '';
-    let humanScore = 0;
-    let computerScore = 0;
-
-
+    // Compare both player's hands and determine a winner
     function determineWinner() {
+        console.log(`Human choice: ${humanChoice}\nComputer choice: ${computerChoice}`);
+
+        if (humanChoice === computerChoice) {
+            itsADraw();
+            return;
+        }
+
         switch (humanChoice) {
-            case humanChoice === computerChoice:
-                itsADraw();
-            case rock:
-                computerChoice === 'paper' ? computerWins() : humanWins();
+            case 'rock':
+                computerChoice === 'paper' ? computerWins(computerChoice) : humanWins(humanChoice);
                 break;
-            case paper:
-                computerChoice === 'scissors' ? computerWins() : humanWins();
+            case 'paper':
+                computerChoice === 'scissors' ? computerWins(computerChoice) : humanWins(humanChoice);
                 break;
-            case scissors:
-                computerChoice === 'rock' ? computerWins() : humanWins();
+            case 'scissors':
+                computerChoice === 'rock' ? computerWins(computerChoice) : humanWins(humanChoice);
                 break;
             default:
                 break;
         }
+
     }
 
-
-    function sayGameChoices() {
-        // victoryStatement = `You picked... ${humanChoice}!\nComputer picked... ${computerChoice}!`;
-        console.log(`You picked... ${humanChoice}!\nComputer picked... ${computerChoice}!`);
-    }
-
-    // Helper functions for clarity
+    // Functions for displaying round results
     function itsADraw() {
-        console.log(`It's a draw!`)
+        console.log(`It's a draw!`);
     }
 
-    function humanWins() {
-        console.log(`You win! ${capitalize(humanChoice)} beats ${computerChoice}!`);
+    let winningHandVerb;
+
+    const humanWins = (winningHand) => {
+        score.humanWins++;
+        getWinningHandVerb(winningHand);
+        console.log(`You won! ${capitalize(humanChoice)} ${winningHandVerb} ${computerChoice}!`);
     }
 
-    function computerWins() {
-        console.log(`You lose! ${capitalize(computerChoice)} beats ${humanChoice}!`);
+    const computerWins = (winningHand) => {
+        score.computerWins++;
+        getWinningHandVerb(winningHand);
+        console.log(`You lost! ${capitalize(computerChoice)} ${winningHandVerb} ${humanChoice}!`);
+    }
+
+    // Set the proper verb for whatever hand won in the round result sentence
+    function getWinningHandVerb(winningHand) {
+
+        switch (winningHand) {
+            case 'rock':
+                winningHandVerb = 'crushes'
+                break;
+            case 'paper':
+                winningHandVerb = 'covers'
+                break;
+            case 'scissors':
+                winningHandVerb = 'cuts'
+                break;
+            default:
+                break;
+        }
+
+        return winningHandVerb;
     }
 
     function capitalize(s) {
         return String(s[0]).toUpperCase() + String(s).slice(1);
     }
+
+    determineWinner();
+
+    // Display the current score
+    console.log(`Score: Human: ${score.humanWins} Computer: ${score.computerWins}`);
 
 }
